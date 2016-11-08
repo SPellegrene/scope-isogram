38c8a35dfe5b1bd145d782c7d2e76f25e6d3e77c
Sam Pellegrene <pellegrenesam@gmail.com>
2016-11-08 14:07:04 -0600

    isogram assignment

--- src/index-copy.js
+++ src/index-copy.js
@@ -9,12 +9,43 @@ isIsogram( "aba" ) == false
 isIsogram( "moOse" ) == false // -- ignore letter case
 **/
 
+ //let the strings in
+// function isIsogram(str){
+//
+// //assign the string to a variable that splits each string into seperate letters and lowercase
+//   var isogram = str.toLowerCase().split('');
+//   return isogram;
+// }
+// //run through each string/letter in isogram
+//   for (var i = 0; i < isogram.length; i++){
+//     return i;
+//   }
+//while looping, if the letter has already been found then make it false
+
+
+// if the letter gets marked as undefined, then it's true
+
+//otherwise return the letter as false
+
 function isIsogram(str){
-  var isogram = ();
-  for (var i =0; i<isogram.length; i++){
-    return isogram;
+  var isogram = {};
+  if (typeof str === "undefined") {
+    return true;
   }
-}
+
+    for (var i=0; i<str.length; i++) {
+      var currentLetter = str[i].toLowerCase();
+        if (isogram[currentLetter]){
+          return false;
+        } else {
+          isogram[currentLetter] = currentLetter;
+        }
+    }
+
+    return(true);
+};
+
+
 
 console.assert( isIsogram("Dermatoglyphics") );
 console.assert( isIsogram("isogram") );
